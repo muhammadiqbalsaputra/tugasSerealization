8c7f34476c5a48c7696bd5d1494a34335c50aa62
muhammadiqbalsaputra <muhammad.iqbal.sap@gmail.com>
2025-04-20 23:14:47 +0700

    first commit

---
+++ src/tugasSerealization/Mahasiswa.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugasSerealization;
+
+import java.io.Serializable;
+
+public class Mahasiswa implements Serializable {
+    private String nim;
+    private String nama;
+    private String kelas;
+
+    public Mahasiswa(String nim, String nama, String kelas) {
+        this.nim = nim;
+        this.nama = nama;
+        this.kelas = kelas;
+    }
+
+    public String getNim() { return nim; }
+    public String getNama() { return nama; }
+    public String getKelas() { return kelas; }
+}
